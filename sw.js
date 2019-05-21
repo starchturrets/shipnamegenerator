7ebaa8bb86d8d9d3a55866b6693146fb7504f1a5
James Thaumiel <starchturrets@protonmail.com>
2019-05-21 20:31:50 +0400

    It works on localhost. A guy can hope. . .

--- sw.js
+++ sw.js
@@ -1,26 +1,37 @@
 //With special thanks to Brad Traversy for his awesome videos
-
-self.addEventListener('install', (event) => {
+const cacheName = 'v1';
+const cacheAssets = [
+	'/',
+	'index.html',
+	'sw.js',
+	'src/css/style.css', //All this time and it was the f***ing path!
+	'src/js/script.js',
+	'src/favicons/apple-touch-icon.png',
+	'src/favicons/favicon-32x32.png',
+	'src/favicons/favicon-16x16.png',
+	'src/favicons/site.webmanifest',
+	'src/favicons/safari-pinned-tab.svg',
+	'src/favicons/favicon.ico'
+];
+/*self.addEventListener('install', (event) => {
 	console.log('Service worker activated.');
 	event.waitUntil(
 		caches
 			.open('v1')
 			.then((cache) => {
 				return cache.addAll([
-					'/',
-					'index.html',
-					'style.css',
-					'script.js',
-					'/src/favicons/apple-touch-icon.png',
-					'/src/favicons/favicon-32x32.png',
-					'/src/favicons/favicon-16x16.png',
-					'/src/favicons/site.webmanifest',
-					'/src/favicons/safari-pinned-tab.svg',
-					'/src/favicons/favicon.ico'
+
 				]);
 			})
 			.then(() => self.skipWaiting())
 			.catch(() => console.log('I have failed. Again'))
+	);
+});*/
+self.addEventListener('install', (event) => {
+	event.waitUntil(
+		caches.open(cacheName).then((cache) => {
+			return cache.addAll(cacheAssets);
+		})
 	);
 });
 /*self.addEventListener('activate', (e) => {
@@ -42,7 +53,7 @@ self.addEventListener('activate', (e) => {
 		caches.keys().then((cacheNames) => {
 			return Promise.all(
 				cacheNames.map((cache) => {
-					if (cache !== 'v1') {
+					if (cache !== cacheName) {
 						console.log('Service Worker: Clearing Old Cache');
 						return caches.delete(cache);
 					}
@@ -53,11 +64,7 @@ self.addEventListener('activate', (e) => {
 });
 
 // Call Fetch Event
-/*self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', (e) => {
 	console.log('Service Worker: Fetching');
 	e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
-*/
-self.addEventListener('fetch', (event) => {
-	event.respondWith(caches.match(event.request));
 });
