7b062c0175931fb75280ccb1527384fbf11e0652
James Thaumiel <starchturrets@protonmail.com>
2019-05-21 17:47:40 +0400

    Added service workers

---
+++ sw.js
@@ -0,0 +1,10 @@
+self.addEventListener('install', (event) => {
+	event.waitUntil(
+		caches.open('v1').then((cache) => {
+			return cache.addAll(['/', 'index.html', 'style.css', 'script.js']);
+		})
+	);
+});
+self.addEventListener('fetch', (event) => {
+	event.respondWith(caches.match(event.request));
+});
