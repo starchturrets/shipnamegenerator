0fe36105381c503e24e6957feb7156ff5d96e33b
James Thaumiel <starchturrets@protonmail.com>
2019-05-21 19:17:59 +0400

    As usual, I have no idea what I'm doing

--- sw.js
+++ sw.js
@@ -1,7 +1,19 @@
 //With special thanks to Brad Traversy for his awesome videos
-const cacheName = 'v3';
-const cacheAssets = ['/', 'index.html', 'style.css', 'script.js'];
+const cacheName = 'Testing';
+const cacheAssets = [
+	'index.html',
+	'style.css',
+	'script.js',
+	'/src/favicons/apple-touch-icon.png',
+	'/src/favicons/favicon-32x32.png',
+	'/src/favicons/favicon-16x16.png',
+	'/src/favicons/site.webmanifest',
+	'/src/favicons/safari-pinned-tab.svg',
+	'/src/favicons/favicon.ico'
+];
+
 self.addEventListener('install', (event) => {
+	console.log('Service worker activated.');
 	event.waitUntil(
 		caches
 			.open(cacheName)
@@ -10,7 +22,7 @@ self.addEventListener('install', (event) => {
 			.catch(() => console.log('I have failed. Again'))
 	);
 });
-self.addEventListener('activate', (e) => {
+/*self.addEventListener('activate', (e) => {
 	e.waitUntil(caches.keys).then((cacheNames) => {
 		return Promise.all(
 			cacheNames.map((cache) => {
@@ -20,11 +32,27 @@ self.addEventListener('activate', (e) => {
 			})
 		);
 	});
-});
-self.addEventListener('fetch', (e) => {
-	e.respondWith(
-		fetch(e.request).catch(() => {
-			caches.match(e.request);
+});*/
+// Call Activate Event
+self.addEventListener('activate', (e) => {
+	console.log('Service Worker: Activated');
+	// Remove unwanted caches
+	e.waitUntil(
+		caches.keys().then((cacheNames) => {
+			return Promise.all(
+				cacheNames.map((cache) => {
+					if (cache !== cacheName) {
+						console.log('Service Worker: Clearing Old Cache');
+						return caches.delete(cache);
+					}
+				})
+			);
 		})
 	);
+});
+
+// Call Fetch Event
+self.addEventListener('fetch', (e) => {
+	console.log('Service Worker: Fetching');
+	e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
