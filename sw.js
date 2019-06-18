e6121096d57fe4db9d63c49e175ff819e7b9fffa
James Thaumiel <starchturrets@protonmail.com>
2019-06-18 11:57:02 +0400

    More service worker debugging

--- sw.js
+++ sw.js
@@ -7,33 +7,33 @@ const cacheAssets = [
   '/sw.js',
   '/manifest.json',
   '/src/css/style.css', //All this time and it was the f***ing path!
-  '/src/js/script.js',
-  '/src/favicons/apple-touch-icon.png',
-  '/src/favicons/favicon-32x32.png',
-  '/src/favicons/favicon-16x16.png',
-  '/src/favicons/site.webmanifest',
-  '/src/favicons/safari-pinned-tab.svg',
-  '/src/favicons/favicon.ico',
-  '/src/favicons/android-chrome-192x192.png',
-  '/src/favicons/android-chrome-512x512.png'
+  '/src/js/script.js'
+  // '/src/favicons/apple-touch-icon.png',
+  // '/src/favicons/favicon-32x32.png',
+  // '/src/favicons/favicon-16x16.png',
+  // '/src/favicons/site.webmanifest',
+  // '/src/favicons/safari-pinned-tab.svg',
+  // '/src/favicons/favicon.ico',
+  // '/src/favicons/android-chrome-192x192.png',
+  // '/src/favicons/android-chrome-512x512.png'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(cacheName).then((cache) => {
+    caches.open(cacheName).then(cache => {
       return cache.addAll(cacheAssets);
     })
   );
 });
 
 // Call Activate Event
-self.addEventListener('activate', (e) => {
+self.addEventListener('activate', e => {
   console.log('Service Worker: Activated');
   // Remove unwanted caches
   e.waitUntil(
-    caches.keys().then((cacheNames) => {
+    caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames.map((cache) => {
+        cacheNames.map(cache => {
           if (cache !== cacheName) {
             console.log('Service Worker: Clearing Old Cache');
             return caches.delete(cache);
@@ -45,7 +45,7 @@ self.addEventListener('activate', (e) => {
 });
 
 // Call Fetch Event
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', e => {
   console.log('Service Worker: Fetching');
   e.respondWith(
     ajax(e.request).catch(() => {
@@ -59,7 +59,7 @@ function ajax(url, timeout = 1000) {
   return new Promise((resolve, reject) => {
     fetch(url)
       // .then((res) => res.json())
-      .then((data) => resolve(data));
+      .then(data => resolve(data));
     setTimeout(() => reject('Request failed'), timeout);
   });
 }
